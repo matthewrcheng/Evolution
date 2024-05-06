b4767c35a152f12a4433afae1880c4b2b34b8355
matthewcheng13 <mcheng.mm@gmail.com>
2024-05-05 20:38:52 -0400

    Began adding diet and reproduction

--- Organism.py
+++ Organism.py
@@ -1,31 +1,41 @@
 import random
 from Cell import *
+from utils import *
 
 directions = [(1,0), (0,1), (-1,0), (0,-1)]
 # Define organism
 class Organism:
-    def __init__(self, MUTATION_RATE, x, y, id):
+    def __init__(self, MUTATION_RATE, x, y, id, health=100, hunger=100, speed=1, turn_ratio=0.8, nourishment=10, heal_limit=90,
+                 diet=Diet.HERBIVORE, repro=Reproduction.ASEXUAL):
         self.mutation_rate = MUTATION_RATE
         self.x = x
         self.y = y
         self.direction = (1,0)
         self.id = id
         self.cells = [MainCell()]
-        self.health = 100
-        self.hunger = 100
-        self.speed = 1
+        self.max_health = health
+        self.max_hunger = hunger
+        self.health = health
+        self.hunger = hunger
+        self.speed = speed
+        self.turn_ratio = turn_ratio
+        self.nourishment = nourishment
+        self.heal_limit = heal_limit
+        self.diet = diet
+        self.reproduction = repro
 
     def turn(self):
-        if random.random() > 0.8:
+        if random.random() > self.turn_ratio:
             self.direction = random.choice(directions)
 
     def eat(self, env):
         x = self.x+self.direction[0]
         y = self.y+self.direction[1]
+        
         if x >= 0 and y >= 0 and x < env.grid_size and y < env.grid_size and env.grid[x][y] == 1:
             env.grid[x][y] = None
-            self.hunger += 10
-            self.hunger = min(self.hunger, 100)
+            self.hunger += self.nourishment
+            self.hunger = min(self.hunger, self.max_hunger)
             print(f"{self.id} ate. New hunger: {self.hunger}")
 
     def move(self, env):
@@ -38,6 +48,12 @@ class Organism:
             self.y = y
             # print(f"{self.id} moved to {self.x},{self.y}")
 
+    def reproduce(self, env):
+        if self.reproduction == Reproduction.ASEXUAL:
+            pass
+        else:
+            pass
+
     def mutate(self):
         if random.random() < self.mutation_rate:
             # Add new cell or modify existing one
@@ -49,9 +65,9 @@ class Organism:
         if self.hunger > 0:
             self.hunger -= 1
             # print(f"{self.id}'s hunger is now {self.health}")
-            if self.hunger > 90:
+            if self.hunger > self.heal_limit:
                 self.health += 1
-                self.health = min(self.health, 100)
+                self.health = min(self.health, self.max_health)
         else:
             self.health -= 1
             print(f"{self.id}'s hunger is 0, taking damage {self.health}")
---
+++ utils.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Diet(Enum):
+    HERBIVORE = 0
+    OMNIVORE = 1
+    CARNIVORE = 2
+
+class Reproduction(Enum):
+    ASEXUAL = 0
+    SEXUAL = 1
+
